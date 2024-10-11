8210088665ec9a26b21406f166104eb30eaf04af
tomb-101 <s5712525@bournemouth.ac.uk>
2024-10-11 11:57:19 +0100

    LAB SESSION TWO

--- lab2.py
+++ lab2.py
@@ -1,13 +1,27 @@
+#Advanced task one
+'''
 def addtoarray(num):
     userarray=[]
     for i in range(1,int(num)+1):
         userarray.append(i)
     return userarray
 userstring=""
-print("Enter a number")
+print("Enter a number (preferrably between 1 and 9)")
 usernum=int(input())
 for i in addtoarray(usernum):
     for n in range(0, i):
         userstring=userstring+str(i)
     print(userstring)
     userstring=""
+'''
+
+#Advanced task two
+
+pointer=0
+print("Enter a number (preferrably large)")
+usernum=int(input())
+
+if type(usernum[pointer])!=int:
+    print(usernum[pointer+1])
+else:
+    print(usernum[pointer])
