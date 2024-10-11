ca04e117259172506516d43fe20b7849c66c2d03
Filip Stefanov (s5712525) <s5712525@bournemouth.ac.uk>
2024-10-11 10:56:14 +0100

    welcom

---
+++ lab2.py
@@ -0,0 +1,14 @@
+#Exercise
+'''
+a = 11
+a+=5
+atype=type(a)
+print(f"a = {a} and the data type is {atype}")
+
+a=11
+b=5
+result = a<b
+type_result=type(result)
+print(f"result= {result} and its data type is {type_result}")
+'''
+
